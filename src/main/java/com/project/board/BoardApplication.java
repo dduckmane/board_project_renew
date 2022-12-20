e8c24fd68759d390055487525e3653820b08e512
minsung <kms199719@naver.com>
2022-12-20 23:45:02 +0900

    feat "awsV1 "

--- src/main/java/com/project/board/BoardApplication.java
+++ src/main/java/com/project/board/BoardApplication.java
@@ -2,12 +2,19 @@ package com.project.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @SpringBootApplication
 
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BoardApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
